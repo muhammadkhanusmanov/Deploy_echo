44a293016d65b2507868dd1451e26e551f472e30
muhammadkhanusmanov <uqaxromon@gmail.com>
2023-02-17 23:58:25 -0600

    app

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+from telegram import Bot, Update
+from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
+import os
+
+
+from bot_main import start, echo
+
+
+TOKEN = os.environ["TOKEN"]
+
+bot = Bot(TOKEN)
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=["POST", "GET"])
+def hello():
+    if request.method == 'GET':
+        return 'hi from Python2022I'
+    elif request.method == "POST":
+        data = request.get_json(force = True)
+        
+        dispacher: Dispatcher = Dispatcher(bot, None, workers=0)
+        update:Update = Update.de_json(data, bot)
+    
+        #update 
+        dispacher.add_handler(CommandHandler('start', callback=start))
+        dispacher.add_handler(MessageHandler(Filters.text, echo))
+        
+        dispacher.process_update(update)
+        return 'ok'
+
+bot=Bot(TOKEN)
